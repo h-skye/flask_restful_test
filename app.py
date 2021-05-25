690f2ec409e059c1a45b36704b425ca63f613238
Skye Huang <shuang@Skyes-MacBook-Pro.local>
2021-05-25 12:26:43 -0400

    updated post item to ensure no duplicates

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ items = []
 class Item(Resource):
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
-        return { 'item': item }, 200 if item else 404
+        return {'item': item}, 200 if item else 404
         
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': 'An item with {} already exists'.format(name)}, 400
+
         data = request.get_json()
         item = {'name': name, 'price': data['price'] }
         items.append(item)
